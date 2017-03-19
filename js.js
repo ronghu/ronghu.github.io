d8be7205306b59954e466d6dd45ed05ab42eee44
ronghu <yinccleta@gmail.com>
2017-03-19 20:26:29 +0800

    Rong's Portfolio

---
+++ js.js
@@ -0,0 +1,7 @@
+$(function(){
+	$("#goTop").click(function(){
+		$("html,body").animate({scrollTop:0},900);
+		//$("html,body").animate({scrollTop:0},900,"easeOutBounce");
+		return false;
+	});
+});

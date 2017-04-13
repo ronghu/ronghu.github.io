cd3f28e9fb6e4c803a9ce867c94784d68a0fafc4
ronghu <yinccleta@gmail.com>
2017-04-13 19:08:15 +0800

    R

--- js.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$("#goTop").click(function(){
-		$("html,body").animate({scrollTop:0},900);
-		//$("html,body").animate({scrollTop:0},900,"easeOutBounce");
-		return false;
-	});
-});
-

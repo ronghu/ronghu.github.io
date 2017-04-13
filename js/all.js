9dbe28ed76e3c0ecdc1cadc14bee61b4beb042b9
ronghu <yinccleta@gmail.com>
2017-04-13 18:10:41 +0800

    Rong's Portfolio
    Multimedia design, Graphic design, Product design

---
+++ js/all.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+    $("#goTop").click(function(){
+		$("html,body").animate({scrollTop:0},900);
+		//$("html,body").animate({scrollTop:0},900,"easeOutBounce");
+		return false;
+	});
+    
+    $('.M1').hover(function(){
+        $(this).css('transition','1s');
+        }, function(){
+        $(this).css('transition','1s');     
+    });
+    
+    $('.M2').hover(function(){
+        $(this).css('transition','1s');
+        }, function(){
+        $(this).css('transition','1s');     
+    });
+    
+    $('.M3').hover(function(){
+        $(this).css('transition','1s');
+        }, function(){
+        $(this).css('transition','1s');     
+    });
+   
+    $('.M4').hover(function(){
+        $(this).css('transition','1s');
+        }, function(){
+        $(this).css('transition','1s');     
+    });
+    
+    $('.M5').hover(function(){
+        $(this).css('transition','1s');
+        }, function(){
+        $(this).css('transition','1s');     
+    });
+    
+    $('.G1').hover(function(){
+        $(this).css('transition','1s');
+        }, function(){
+        $(this).css('transition','1s');     
+    });
+    
+    $('.G2').hover(function(){
+        $(this).css('transition','1s');
+        }, function(){
+        $(this).css('transition','1s');     
+    });
+    
+    $('.G3').hover(function(){
+        $(this).css('transition','1s');
+        }, function(){
+        $(this).css('transition','1s');     
+    });
+    
+    $('.P1').hover(function(){
+        $(this).css('transition','1s');
+        }, function(){
+        $(this).css('transition','1s');     
+    });
+    
+    $('.P2').hover(function(){
+        $(this).css('transition','1s');
+        }, function(){
+        $(this).css('transition','1s');     
+    });
+    
+    $('.P3').hover(function(){
+        $(this).css('transition','1s');
+        }, function(){
+        $(this).css('transition','1s');     
+    });
+});
